32bf423c8821ff6ed518ab049ffdfe1f6269c38f
Shiyao Wu <shiyaowu@usc.edu>
2014-07-21 14:48:26 -0700

    Added rule for Fizz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
- int main int argc,char*argv[]){
-
-for (unsigned int i=0; i<100; i++){
-std::cout<<i<<std::endl; 
-}
-return (0); 
+#include <iostream>
+ 
+int main(int argc, char *argv[]) {
+	for(unsigned int i = 0; i < 100; i++) {
+		if( i % 3 == 0)
+			std::cout << i << ": " << "Fizz" << std::endl;
+	}
+	
+	return(0);
 }
